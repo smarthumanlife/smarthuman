8290696c9e8e0a8c15334b998ee1764d229fb5bd
smarthumanlife <150536674+smarthumanlife@users.noreply.github.com>
2024-01-23 00:07:29 +0900

    Update AWSUpbit.py

--- AWSUpbit/AWSUpbit.py
+++ AWSUpbit/AWSUpbit.py
@@ -1,1 +1,68 @@
-rrt
+import time
+import pyupbit
+import datetime
+
+access = "oZqPpIejx3ZzYGsRByGyC0vlWS8iXpTcG4z2YfzQ"
+secret = "xiIUMc9fcxqs4NlNRxCoG3qN67VUr8yhb4CvW1eM"
+
+def get_target_price(ticker, k):
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
+    return target_price
+
+def get_start_time(ticker):
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    start_time = df.index[0]
+    return start_time
+
+def get_ma15(ticker):
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=15)
+    ma15 = df['close'].rolling(15).mean().iloc[-1]
+    return ma15
+
+def get_balance(ticker):
+    balances = upbit.get_balances()
+    for b in balances:
+        if b['currency'] == ticker:
+            if b['balance'] is not None:
+                return float(b['balance'])
+            else:
+                return 0
+    return 0
+
+def get_current_price(ticker):
+    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
+
+upbit = pyupbit.Upbit(access, secret)
+print("autotrade start")
+
+while True:
+    try:
+        now = datetime.datetime.now()
+        start_time = get_start_time("KRW-BTC")
+        end_time = start_time + datetime.timedelta(days=1)
+
+        print(f"Now: {now}, Start Time: {start_time}, End Time: {end_time}")
+
+        if start_time < now < end_time - datetime.timedelta(seconds=10):
+            target_price = get_target_price("KRW-BTC", 0.5)
+            ma15 = get_ma15("KRW-BTC")
+            current_price = get_current_price("KRW-BTC")
+
+            print(f"Target Price: {target_price}, MA15: {ma15}, Current Price: {current_price}")
+
+            if target_price < current_price and ma15 < current_price:
+                krw = get_balance("KRW")
+                if krw > 5000:
+                    print(f"Buying BTC with {krw} KRW")
+                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
+        else:
+            btc = get_balance("BTC")
+            if btc > 0.00008:
+                print(f"Selling {btc} BTC")
+                upbit.sell_market_order("KRW-BTC", btc*0.9995)
+
+        time.sleep(1)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        time.sleep(1)
